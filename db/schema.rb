ee42b38a0f57c3c008ab98a1755cb2a3355f0a67
Philip de la Cal <pdelacal@gmail.com>
2017-02-14 17:18:45 -0500

    adding ID to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214220055) do
+ActiveRecord::Schema.define(version: 20170214220510) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "title"
-    t.string   "body"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
