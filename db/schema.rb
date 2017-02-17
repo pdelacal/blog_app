c93c3f6f887b1e8cb96e1f5b1184eaf686bc0583
Philip de la Cal <pdelacal@gmail.com>
2017-02-17 18:48:46 -0500

    reset with rake

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214220510) do
+ActiveRecord::Schema.define(version: 20170214221627) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
